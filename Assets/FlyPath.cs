4286b993f5db66718b57b1e4983bc6514e25ee76
Nhungvu439 <146834474+nhungvu05@users.noreply.github.com>
2026-03-22 01:22:56 +0700

    demo 8

---
+++ Assets/FlyPath.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FlyPath : MonoBehaviour
+{
+    public Waypoint[] waypoints;
+    public Vector3 this[int index] => waypoints[index].transform.position;
+    private void Reset() => waypoints = GetComponentsInChildren<Waypoint>();
+
+    private void OnDrawGizmos()
+    {
+        if (waypoints == null || waypoints.Length < 2) return;
+        Gizmos.color = Color.green;
+        for (int i = 0; i < waypoints.Length - 1; i++)
+        {
+            if (waypoints[i] != null && waypoints[i + 1] != null)
+            {
+                Gizmos.DrawLine(waypoints[i].transform.position, waypoints[i + 1].transform.position);
+            }
+        }
+    }
+}
---
+++ Assets/FlyPathAgent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class FlyPathAgent : MonoBehaviour
+{
+    public FlyPath flyPath;
+    public float flySpeed;
+    private int nextIndex = 1;
+
+    void Start() => transform.position = flyPath[0];
+
+    void Update()
+    {
+        if (flyPath == null) return;
+        if (nextIndex >= flyPath.waypoints.Length) return;
+        if (transform.position != flyPath[nextIndex])
+        {
+            FlyToNextWaypoint();
+        }
+        else
+        {
+            nextIndex++;
+        }
+    }
+
+    private void FlyToNextWaypoint()
+        => transform.position = Vector3.MoveTowards(transform.position, flyPath[nextIndex], flySpeed * Time.deltaTime);
+}
